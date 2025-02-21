72f3f3776e97c8d51aecbf86132b9d5db9c4c65d
Celia-kffi <120136130+Celia-kffi@users.noreply.github.com>
2025-02-21 09:09:55 +0100

    Ajout dans XmartCityService

--- xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/XMartCityService.java
+++ xmart-city-backend/src/main/java/edu/ezip/ing1/pds/business/server/XMartCityService.java
@@ -1,121 +1,96 @@
 package edu.ezip.ing1.pds.business.server;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.ezip.ing1.pds.business.dto.Client;
-import edu.ezip.ing1.pds.business.dto.Clients;
-import edu.ezip.ing1.pds.commons.Request;
-import edu.ezip.ing1.pds.commons.Response;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import edu.ezip.ing1.pds.business.dto.AvisClient;
+import edu.ezip.ing1.pds.business.dto.AvisClients;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.ezip.ing1.pds.commons.Request;
+import edu.ezip.ing1.pds.commons.Response;
 
 public class XMartCityService {
 
-    private final static String LoggingLabel = "B u s i n e s s - S e r v e r";
+    private final static String LoggingLabel = "Business - Server";
     private final Logger logger = LoggerFactory.getLogger(LoggingLabel);
 
     private enum Queries {
-        SELECT_ALL_CLIENTS("SELECT id_client, nom, prenom, age, nationalite FROM clients"),
-        INSERT_CLIENT("INSERT INTO clients (nom, prenom, age, nationalite) VALUES (?, ?, ?, ?)"),
-        DELETE_CLIENT("DELETE FROM clients WHERE id_client = ?");
+        SELECT_ALL_AVIS("SELECT id_avis, note, date_avis, commentaires, id_client FROM avis_clients"),
+        INSERT_AVIS("INSERT INTO avis_clients (note, date_avis, commentaires, id_client) VALUES (?, ?, ?, ?)");
 
         private final String query;
 
-        Queries(final String query) {
+        private Queries(final String query) {
             this.query = query;
         }
     }
 
     public static XMartCityService inst = null;
-
-    public static XMartCityService getInstance() {
+    public static final XMartCityService getInstance() {
         if (inst == null) {
             inst = new XMartCityService();
         }
         return inst;
     }
 
-    private XMartCityService() {
-    }
+    private XMartCityService() {}
 
     public final Response dispatch(final Request request, final Connection connection)
             throws InvocationTargetException, IllegalAccessException, SQLException, IOException {
         Response response = null;
-
         final Queries queryEnum = Enum.valueOf(Queries.class, request.getRequestOrder());
         switch (queryEnum) {
-            case SELECT_ALL_CLIENTS:
-                response = selectAllClients(request, connection);
+            case SELECT_ALL_AVIS:
+                response = selectAllAvis(request, connection);
                 break;
-            case INSERT_CLIENT:
-                response = insertClient(request, connection);
-                break;
-            case DELETE_CLIENT:
-                response = deleteClient(request, connection);
+            case INSERT_AVIS:
+                response = insertAvis(request, connection);
                 break;
             default:
-                logger.warn("Requête inconnue : {}", request.getRequestOrder());
                 break;
         }
-
         return response;
     }
 
-    private Response selectAllClients(final Request request, final Connection connection) throws SQLException, JsonProcessingException {
+    private Response insertAvis(final Request request, final Connection connection) throws SQLException, IOException {
         final ObjectMapper objectMapper = new ObjectMapper();
-        final Statement stmt = connection.createStatement();
-        final ResultSet res = stmt.executeQuery(Queries.SELECT_ALL_CLIENTS.query);
-        Clients clients = new Clients();
+        final AvisClient avisClient = objectMapper.readValue(request.getRequestBody(), AvisClient.class);
 
-        while (res.next()) {
-            Client client = new Client();
-            client.setIdClient(res.getInt("id_client"));
-            client.setNom(res.getString("nom"));
-            client.setPrenom(res.getString("prenom"));
-            client.setAge(res.getInt("age"));
-            client.setNationalite(res.getString("nationalite"));
-            clients.add(client);
-        }
-
-        return new Response(request.getRequestId(), objectMapper.writeValueAsString(clients));
-    }
-
-    private Response insertClient(final Request request, final Connection connection) throws SQLException, IOException {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        final Client client = objectMapper.readValue(request.getRequestBody(), Client.class);
-
-        final PreparedStatement stmt = connection.prepareStatement(Queries.INSERT_CLIENT.query, Statement.RETURN_GENERATED_KEYS);
-        stmt.setString(1, client.getNom());
-        stmt.setString(2, client.getPrenom());
-        stmt.setInt(3, client.getAge());
-        stmt.setString(4, client.getNationalite());
+        final PreparedStatement stmt = connection.prepareStatement(Queries.INSERT_AVIS.query);
+        stmt.setInt(1, avisClient.getNote());
+        stmt.setString(2, avisClient.getDateAvis());
+        stmt.setString(3, avisClient.getCommentaires());
+        stmt.setInt(4, avisClient.getIdClient());
         stmt.executeUpdate();
 
-        final ResultSet res = stmt.getGeneratedKeys();
-        if (res.next()) {
-            client.setIdClient(res.getInt(1)); // Récupère l'ID généré
-        }
-
-        return new Response(request.getRequestId(), objectMapper.writeValueAsString(client));
+        return new Response(request.getRequestId(), objectMapper.writeValueAsString(avisClient));
     }
 
-    private Response deleteClient(final Request request, final Connection connection) throws SQLException, IOException {
+    private Response selectAllAvis(final Request request, final Connection connection) throws SQLException, JsonProcessingException {
         final ObjectMapper objectMapper = new ObjectMapper();
-        final Client client = objectMapper.readValue(request.getRequestBody(), Client.class);
-
-        final PreparedStatement stmt = connection.prepareStatement(Queries.DELETE_CLIENT.query);
-        stmt.setInt(1, client.getIdClient());
-        int affectedRows = stmt.executeUpdate();
-
-        if (affectedRows == 0) {
-            logger.warn("Aucun client supprimé avec ID {}", client.getIdClient());
-            return new Response(request.getRequestId(), "{\"message\": \"Aucun client supprimé.\"}");
+        final Statement stmt = connection.createStatement();
+        final ResultSet res = stmt.executeQuery(Queries.SELECT_ALL_AVIS.query);
+        AvisClients avisClients = new AvisClients();
+        while (res.next()) {
+            AvisClient avisClient = new AvisClient();
+            avisClient.setIdAvis(res.getInt("id_avis"));
+            avisClient.setNote(res.getInt("note"));
+            avisClient.setDateAvis(res.getString("date_avis"));
+            avisClient.setCommentaires(res.getString("commentaires"));
+            avisClient.setIdClient(res.getInt("id_client"));
+            avisClients.add(avisClient);
         }
-
-        return new Response(request.getRequestId(), "{\"message\": \"Client supprimé avec succès.\"}");
+        return new Response(request.getRequestId(), objectMapper.writeValueAsString(avisClients));
     }
 }

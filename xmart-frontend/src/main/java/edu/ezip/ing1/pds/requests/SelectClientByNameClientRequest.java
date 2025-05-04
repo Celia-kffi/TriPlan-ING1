dd9f0f42c53f0689b74fe149f13567cb3b8e6df9
Roumayssae <romyelkenz7@gmail.com>
2025-05-04 16:06:03 +0200

    Ajout de la classe pour récuperer un client par nom

---
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/requests/SelectClientByNameClientRequest.java
@@ -0,0 +1,28 @@
+package edu.ezip.ing1.pds.requests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ezip.ing1.pds.business.dto.Clients;
+import edu.ezip.ing1.pds.client.commons.ClientRequest;
+import edu.ezip.ing1.pds.client.commons.NetworkConfig;
+import edu.ezip.ing1.pds.commons.Request;
+
+import java.io.IOException;
+
+public class SelectClientByNameClientRequest extends ClientRequest<Object, Clients> {
+
+    public SelectClientByNameClientRequest(
+            NetworkConfig networkConfig, int myBirthDate, Request request, Object info, byte[] bytes)
+            throws IOException {
+        super(networkConfig, myBirthDate, request, info, bytes);
+    }
+
+
+    @Override
+    public Clients readResult(String body) throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final Clients clients = mapper.readValue(body, Clients.class);
+        return clients;
+
+        }
+    }
+
